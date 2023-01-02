f3d5d87891c8ee8f017e471619558131514f6170
Fradi575 <78791893+Fradi575@users.noreply.github.com>
2023-01-02 21:31:09 +0100

    vissza beírásával visszanavigál a kk oldalra
    vissza beírásával visszanavigál a kk oldalra

---
+++ kkErdekessegek.js
@@ -0,0 +1,14 @@
+function vissza() {
+    var vissza = document.getElementsByTagName("textarea")[0];
+    if(vissza.value === 'vissza') 
+    open("https://fradi575.github.io/keletiKiralyKobra/");
+    close();
+}
+
+var input = document.getElementById("myInput");
+
+input.addEventListener("keypress", function(event) {
+  
+  if (event.key === "Enter") vissza();
+
+});
