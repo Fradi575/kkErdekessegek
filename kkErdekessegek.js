95f5dba69b0a2d6bfad81d0c249d9f911abf04a8
Fradi575 <78791893+Fradi575@users.noreply.github.com>
2023-01-03 20:08:19 +0100

    Bugok kijavítása.
    Bugok kijavítása.

--- kkErdekessegek.js
+++ kkErdekessegek.js
@@ -1,8 +1,10 @@
 function vissza() {
     var vissza = document.getElementsByTagName("textarea")[0];
-    if(vissza.value === 'vissza') 
-    open("https://fradi575.github.io/keletiKiralyKobra/");
-    close();
+    if(vissza.value === 'vissza') {
+        open("https://fradi575.github.io/keletiKiralyKobra/");
+        close();
+       }
+    
 }
 
 var input = document.getElementById("myInput");
